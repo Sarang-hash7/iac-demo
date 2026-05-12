c66e5779e8259c2a928c3825ef28bb15e91eb065
Sarang Gupta <sarang.gupta@cloud4c.com>
2026-05-12 11:21:23 +0530

    fix:automation account role assignment

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -3,19 +3,20 @@ resource "azurerm_automation_account" "this" {
   location            = var.location
   resource_group_name = var.resource_group
   sku_name            = "Basic"
+
   identity {
     type = "SystemAssigned"
   }
+
   tags = var.tags
 }
 
-data "azurerm_role_definition" "vm_contrib" {
-  name = "Virtual Machine Contributor"
-}
-
 resource "azurerm_role_assignment" "automation_vm_contrib" {
-  scope              = var.resource_group_id
-  role_definition_id = data.azurerm_role_definition.vm_contrib.id
-  principal_id       = azurerm_automation_account.this.identity[0].principal_id
-  depends_on         = [azurerm_automation_account.this]
+  scope                = var.resource_group_id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_automation_account.this.identity[0].principal_id
+
+  depends_on = [
+    azurerm_automation_account.this
+  ]
 }
