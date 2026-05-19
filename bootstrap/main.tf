8276499d518a4dd4c6f596c615a9dc14232d0a58
Sarang Gupta <sarang.gupta@cloud4c.com>
2026-05-19 16:27:06 +0530

    feat:backend network hardening

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -21,7 +21,22 @@ resource "azurerm_storage_account" "tfstate" {
 
   min_tls_version = "TLS1_2"
 
+  public_network_access_enabled = true
+
   allow_nested_items_to_be_public = false
+
+  network_rules {
+    default_action = "Deny"
+
+    ip_rules = [
+      "103.241.182.128",
+      "20.244.30.191"
+    ]
+
+    bypass = [
+      "AzureServices"
+    ]
+  }
 
   blob_properties {
 
