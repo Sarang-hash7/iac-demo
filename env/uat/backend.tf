f9a672111146b39f578c67081efe153b9780e126
Sarang Gupta <sarang.gupta@cloud4c.com>
2026-05-20 12:41:23 +0530

    fix:storage backend accessv2

--- env/uat/backend.tf
+++ env/uat/backend.tf
@@ -4,6 +4,5 @@ terraform {
     storage_account_name = "iacdemouat86503"
     container_name       = "tfstate"
     key                  = "projects/iacdemo/environments/uat/terraform.tfstate"
-    use_azuread_auth     = true # ← forces Azure AD token for data plane
   }
 }
