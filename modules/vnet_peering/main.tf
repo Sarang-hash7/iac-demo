2c07a926dfbd7dacf5592bfbca697e0ea6d589ae
Sarang Gupta <sarang.gupta@cloud4c.com>
2026-05-26 11:51:42 +0530

    feat: Added Vnet Peering

---
+++ modules/vnet_peering/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_virtual_network_peering" "a_to_b" {
+  name                      = "${var.vnet_a_name}-to-${var.vnet_b_name}"
+  resource_group_name       = var.vnet_a_resource_group
+  virtual_network_name      = var.vnet_a_name
+  remote_virtual_network_id = var.vnet_b_id
+
+  allow_virtual_network_access = true
+}
+
+resource "azurerm_virtual_network_peering" "b_to_a" {
+  name                      = "${var.vnet_b_name}-to-${var.vnet_a_name}"
+  resource_group_name       = var.vnet_b_resource_group
+  virtual_network_name      = var.vnet_b_name
+  remote_virtual_network_id = var.vnet_a_id
+
+  allow_virtual_network_access = true
+}
