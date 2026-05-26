369d09a2f3f4f3d1adff1451550b4c3f1324bc04
Sarang Gupta <sarang.gupta@cloud4c.com>
2026-05-26 15:13:30 +0530

    Added Private DNS Zone

--- modules/private_dns/main.tf
+++ modules/private_dns/main.tf
@@ -1,3 +1,15 @@
-# Placeholder module for private DNS / private endpoint integration
-variable "resource_group" { type = string }
-variable "location" { type = string }
+resource "azurerm_private_dns_zone" "this" {
+  name                = var.dns_zone_name
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "links" {
+  for_each = var.vnet_links
+
+  name                  = each.value.name
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.this.name
+  virtual_network_id    = each.value.vnet_id
+
+  registration_enabled = false
+}
--- modules/private_dns/outputs.tf
+++ modules/private_dns/outputs.tf
@@ -1,3 +1,7 @@
-output "private_dns_zone_id" {
-  value = ""
+output "dns_zone_id" {
+  value = azurerm_private_dns_zone.this.id
+}
+
+output "dns_zone_name" {
+  value = azurerm_private_dns_zone.this.name
 }
